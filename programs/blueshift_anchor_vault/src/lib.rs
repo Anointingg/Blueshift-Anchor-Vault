4dfe0028676115628f146d35de35352a1ba765ef
yorubad-dev <adetoyeanointing15@gmail.com>
2025-10-28 14:58:38 +0100

    system vault: add deposit program * checks against double deposit * checks amount greater than the minimum rent before depositing to avoid closing of block on chain

--- programs/blueshift_anchor_vault/src/lib.rs
+++ programs/blueshift_anchor_vault/src/lib.rs
@@ -4,10 +4,45 @@ declare_id!("6FtZmVscYhGX4riXgpjyL1hs5ZERamm5JAR3GuTYbhXd");
 
 #[program]
 pub mod blueshift_anchor_vault {
+    use anchor_lang::system_program::{transfer, Transfer};
+
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+
+    pub fn deposit(ctx: Context<VaultAction>, amount: u64) -> Result<()> {
+        // deposit logic
+        require_eq!(
+            ctx.accounts.vault.lamports(),
+            0,
+            VaultError::VaultAlreadyExist
+        );
+
+        require_gt!(
+            amount,
+            Rent::get()?.minimum_balance(0),
+            VaultError::InvalidAmount
+        );
+
+        transfer(
+            CpiContext::new(
+                ctx.accounts.system_program.to_account_info(),
+                Transfer {
+                    from: ctx.accounts.signer.to_account_info(),
+                    to: ctx.accounts.vault.to_account_info(),
+                },
+            ),
+            amount,
+        )?;
+
+        Ok(())
+    }
+
+    pub fn withdraw(ctx: Context<VaultAction>) -> Result<()> {
+        // withdraw logic
         Ok(())
     }
 }
@@ -22,7 +57,7 @@ pub struct VaultAction<'info> {
     #[account(
         mut,
         seeds = [b"vault", signer.key().as_ref()],
-        bump
+        bump // what does bump do and why is it needed?
     )]
     vault: SystemAccount<'info>,
     system_program: Program<'info, System>,
@@ -34,5 +69,5 @@ pub enum VaultError {
     InvalidAmount,
 
     #[msg("vault already exist")]
-    VaultAlreadyExist
+    VaultAlreadyExist,
 }
